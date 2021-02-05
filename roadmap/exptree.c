c477f7a5e4a0f420c8c92643e9b248af0d3e563f
Harinandan <harinandan.alukkal@gmail.com>
2021-02-05 09:52:40 +0530

    Stage 3: Compile Time Error Detection Implemented

--- roadmap/exptree.c
+++ roadmap/exptree.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 
 #ifndef DATA_H
 #define DATA_H
@@ -18,19 +19,30 @@ tnode *makeConnectorNode(tnode *l, tnode *r)
 tnode *makeReadNode(tnode *target)
 {
 	tnode *temp;
-	temp = (tnode *)malloc(sizeof(tnode));
-	temp->nodetype = 2;
-	temp->left = target;
-	temp->right = NULL;
+
+	if(target -> nodetype == 4){
+		temp = (tnode *)malloc(sizeof(tnode));
+		temp->nodetype = 2;
+		temp->left = target;
+		temp->right = NULL;
+	} else {
+		printf("CAN ONLY READ TO A VARIABLE\n");
+		exit(-1);
+	}
 }
 
 tnode *makeWriteNode(tnode *source)
 {
-	tnode *temp;
-	temp = (tnode *)malloc(sizeof(tnode));
-	temp->nodetype = 1;
-	temp->left = source;
-	temp->right = NULL;
+	if((source -> nodetype == 5 && source->metatype == 1) || (source->nodetype == 4) || (source->nodetype == 3)) {
+		tnode *temp;
+		temp = (tnode *)malloc(sizeof(tnode));
+		temp->nodetype = 1;
+		temp->left = source;
+		temp->right = NULL;
+	} else {
+		printf("ILLEGAL WRITE STATEMENT\n");
+		exit(-1);
+	}
 }
 
 tnode *makeLeafNode(int type, char *s)
@@ -59,6 +71,23 @@ tnode *makeLeafNode(int type, char *s)
 
 tnode *makeOperatorNode(int meta, char *c, tnode *l, tnode *r)
 {
+	if(strcmp(c, "=") == 0){
+		if((l->nodetype != 4) || (r->nodetype !=3 && r->nodetype != 4 && r->nodetype != 5) || (r->nodetype == 5 && r->metatype != 1)) {
+			printf("ILLEGAL ASSINGMENT OPERATION\n");
+			exit(-1);
+		}
+	} else if(meta == 2) {
+		if((l->nodetype != 3 && l->nodetype != 4 && l->nodetype != 5) || (r->nodetype != 3 && r->nodetype != 4 && r->nodetype != 5)) {
+			printf("INCORRECT COMPARISON OPERATION\n");
+			exit(-1);
+		}
+	} else {
+		if((l->nodetype == 5 && r->nodetype == 5 && (l->metatype != 4 || r->metatype !=4))) {
+			printf("CANNOT MIX BOOLEAN EXPRESSION WITH ARITHMETIC EXPRESSION\n");
+			exit(-1);
+		}
+	}
+
 	tnode *temp;
 	temp = (tnode *)malloc(sizeof(tnode));
 	temp->op = c;
@@ -71,21 +100,29 @@ tnode *makeOperatorNode(int meta, char *c, tnode *l, tnode *r)
 
 tnode *makeIfNode(tnode *condn, tnode *trueBody, tnode *falseBody)
 {
-	tnode *temp;
-	temp = (tnode *)malloc(sizeof(tnode));
-	temp->nodetype = 6;
-	temp->left = condn;
-	temp->right = makeConnectorNode(trueBody, falseBody);
-	return temp;
+	if(condn -> nodetype == 5 && condn->metatype == 2) {
+		tnode *temp;
+		temp = (tnode *)malloc(sizeof(tnode));
+		temp->nodetype = 6;
+		temp->left = condn;
+		temp->right = makeConnectorNode(trueBody, falseBody);
+		return temp;
+	} else {
+		printf("ILLEGAL CONDITION STATEMENT\n");
+		exit(-1);
+	}
 };
 
 tnode *makeWhileNode(tnode *condn, tnode *body)
 {
-	tnode *temp;
-	temp = (tnode *)malloc(sizeof(tnode));
-	temp->nodetype = 7;
-	temp->left = condn;
-	temp->right = body;
+	if(condn -> nodetype == 5 && condn->metatype == 2) {
+		tnode *temp;
+		temp = (tnode *)malloc(sizeof(tnode));
+		temp->nodetype = 7;
+		temp->left = condn;
+		temp->right = body;
+	} else {
+		printf("ILLEGAL CONDITION STATEMENT\n");
+		exit(-1);
+	}
 }
-
-// IMPLEMENT CHECKING OF NODES INCLUDING ASSIGNMENT STATEMENT
---
+++ roadmap/run.sh
@@ -0,0 +1,5 @@
+./parser $1
+./label
+cd ../xsm_expl
+./xsm -l library.lib -e ../roadmap/final.xsm
+cd ../roadmap
